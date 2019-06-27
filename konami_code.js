fe884e048b3839b48fb1f46df1bdd33855e2b326
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-06-27 17:51:57 +0100

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
+let index = 0;
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  document.body.addEventListener("keydown", function(event) {
+    const key = parseInt(event.detail || event.ehich);
+
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length) {
+        alert("Congratulations, you made it!");
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  })
 
 }
